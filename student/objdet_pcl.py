bf29ec33fb0f5d8aa5dd4f8a361cd86c83e70869
Arun Sundaresan <arurama@gmail.com>
2022-04-08 08:06:27 +0530

    Update in object_dectection_pcl

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -76,16 +76,31 @@ def show_range_image(frame, lidar_name):
     print("student task ID_S1_EX1")
 
     # step 1 : extract lidar data and range image for the roof-mounted lidar
+# load range image
+   lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0] # get laser data structure from frame
+   if len(lidar.ri_return1.range_image_compressed) > 0: # use first response
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
     
     # step 2 : extract the range and the intensity channel from the range image
+        range_channel     = ri[:,:,0]
+		intensity_channel = ri[:,:,1]
     
     # step 3 : set values <0 to zero
-    
-    # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
+    range_channel[range_channel<0]=0
+    intensity_channel[intensity_channel<0]=0
+     
+    # step 4 : map the range channel onto an 8-bit scale (gray-scale) and make sure that the full range of values is appropriately considered
+    ri_range = range_ch * 255. / (np.amax(range_ch) - np.amin(range_ch))
+    img_range = ri_range.astype(np.uint8)
     
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
+    intensity_channel = intensity_channel * 255. / (np.percentile(intensity_channel,99) - np.percentile(intensity_channel,1))
+    img_intensity = intensity_channel.astype(np.uint8)
     
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
+     img_range_intensity = np.vstack((img_range, img_intensity))
     
     img_range_intensity = [] # remove after implementing all steps
     #######
@@ -112,13 +127,17 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX1")
 
     ## step 1 :  compute bev-map discretization by dividing x-range by the bev-image height (see configs)
+    discretization = (configs.lim_x[1] - configs.lim_x[0]) / configs.bev_height
 
-    ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates    
+    ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates
+    lidar_pcl_cpy = lidar_pcl.copy()
+    lidar_pcl_cpy[:, 0] /= discretization
 
     # step 3 : perform the same operation as in step 2 for the y-coordinates but make sure that no negative bev-coordinates occur
-
-    # step 4 : visualize point-cloud using the function show_pcl from a previous task
+    lidar_pcl_cpy[:, 1] = np.int_(np.floor(lidar_pcl_cpy[:, 1] / discretization) + (configs.discretization + 1) / 2)
     
+    # step 4 : visualize point-cloud using the function show_pcl from a previous task
+    show_pcl(lidar_pcl_cpy)
     #######
     ####### ID_S2_EX1 END #######     
     
@@ -129,18 +148,29 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX2")
 
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    intensity_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
 
     # step 2 : re-arrange elements in lidar_pcl_cpy by sorting first by x, then y, then -z (use numpy.lexsort)
+    idx_height = np.lexsort((-lidar_pcl_cpy[:, 2], lidar_pcl_cpy[:, 1], lidar_pcl_cpy[:, 0]))
+    lidar_pcl_height = lidar_pcl_cpy[idx_height]
 
     ## step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
     ##          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
-
+    _, indices, count = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True, return_counts=True)
+    lidar_pcl_height = lidar_pcl_cpy[indices]
+    
+    
     ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
     ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
     ##          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
+    intensity_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = lidar_pcl_top[:, 3] / (np.percentile(lidar_pcl_top[:, 3],99)-np.percentile(lidar_pcl_top[:, 3],1))
 
     ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
-
+    img_intensity = intensity_map * 256
+    img_intensity = img_intensity.astype(np.uint8)
+    cv2.imshow('img_intensity', img_intensity)
+    cv2.destroyAllWindows()
+    
     #######
     ####### ID_S2_EX2 END ####### 
 
@@ -151,13 +181,20 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX3")
 
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
-
+    height_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
+    
     ## step 2 : assign the height value of each unique entry in lidar_top_pcl to the height map 
     ##          make sure that each entry is normalized on the difference between the upper and lower height defined in the config file
     ##          use the lidar_pcl_top data structure from the previous task to access the pixels of the height_map
-
+    height_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = lidar_pcl_top[:, 2] / float(np.abs(configs.lim_z[1] - configs.lim_z[0]))
+    
     ## step 3 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
-
+    img_height = height_map * 256
+    img_height = img_height.astype(np.uint8)
+    cv2.imshow('img_height', img_height)
+    cv2.imwrite('img_height.png', img_height)
+    cv2.destroyAllWindows()
+    
     #######
     ####### ID_S2_EX3 END #######       
 
